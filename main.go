9af6c22dd37f012eef54ed379950c67ed3c61a8c
IFloatingCloudI <135905947+IFloatingCloudI@users.noreply.github.com>
2025-08-24 15:57:09 +0300

    test2

--- main.go
+++ main.go
@@ -9,12 +9,11 @@ func aboba() string {
 func main() {
 	fmt.Println("test branch in git")
 	fmt.Println(aboba())
-	//ololo
-	//test2
-	//branchtest
+
 	fmt.Println(2 * 2 * 2)
-	//git main comm
 	var a int
 	a = 5
 	fmt.Println(a)
+	fmt.Println("adasd")
+
 }
